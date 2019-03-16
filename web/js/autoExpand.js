a4e569c7066bff6e2e3d5de750193fe442d4bc82
Juba <jub.saadi@gmail.com>
2019-03-16 02:05:52 +0100

    Je suis bien fatigué :+1:
    Ajout fichier js (auto expand & submitArea)

---
+++ web/js/autoExpand.js
@@ -0,0 +1,22 @@
+document.addEventListener('input', function (event){
+  if(event.target.tagName.toLowerCase() === 'textarea')
+    autoExpand(event.target);
+},false);
+
+var autoExpand = function(field){
+  
+  field.style.height = '0px';
+  var computed = window.getComputedStyle(field);
+  
+  var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+	             + parseInt(computed.getPropertyValue('padding-top'), 10)
+	             + field.scrollHeight
+	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+  
+  field.style.height = height + 'px';
+}
+
+                          
+                          
+             
