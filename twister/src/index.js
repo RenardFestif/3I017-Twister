d8f716ab273899aaec52be9a54d08c70602069d3
romane <romane@192.168.0.28>
2019-03-23 11:37:00 +0100

    galere n°1

---
+++ twister/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/acceuil.css';
+import './css/login.css';
+import './css/connexion.css';
+import MainPage from "./jsx/acceuil"
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<MainPage name="Toto"/>, document.getElementById('root'));
+serviceWorker.unregister();
