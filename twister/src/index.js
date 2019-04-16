661069198db47964b99e4b4b76bc01ba1edcaa3e
romane <romane@192.168.0.28>
2019-04-16 12:16:52 +0200

    rajout du css

--- twister/src/index.js
+++ twister/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/acceuil.css';
-import './css/login.css';
-import './css/connexion.css';
-import './css/perso.css';
+import './css/acceuil_bootstrap.css';
+import './css/inscription_bootstrap.css';
+import './css/connexion_bootstrap.css';
+import './css/acceuil_perso_bootstrap.css';
+import './css/bootstrap.css';
 import './images/logo.png';
 import MainPage from "./jsx/mainpage";
 import * as serviceWorker from './serviceWorker';
--- twister/src/jsx/acceuil.js
+++ twister/src/jsx/acceuil.js
@@ -5,12 +5,14 @@ class Acceuil extends Component {
     
     render(){
         return (
-            <div className="Acceuil">
-				<div id="container" className="">
-					<img id="logo" src={logo} alt="logo"/>
-					<p id="title">Twister</p>
-					<button type="button" className="button" onClick={()=> this.props.changepage("inscription")}>Inscription</button>
-					<button type="button" className="button" onClick={()=> this.props.changepage("connexion")}>Connexion</button>
+            <div className="container-fluid">
+				<div className="row haut">
+					<img className="logoAc" src={logo} alt="logo"/>
+					<h2 className="display-2 titre">Twister</h2>
+				</div>
+				<div className="row bas">
+					<button type="button" className="btn btn-success btn-sm button rounded-pill btnAc" onClick={()=> this.props.changepage("inscription")}>Inscription</button>
+					<button type="button" className="btn btn-success btn-sm button rounded-pill btnAc" onClick={()=> this.props.changepage("connexion")}>Connexion</button>
 				</div>
 			</div>
             );
